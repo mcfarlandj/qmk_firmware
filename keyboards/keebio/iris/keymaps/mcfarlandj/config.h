c8f38c664447508e5dc8e770035f19ee5f214052
Jason McFarland <jason.mcfarland@jumpcloud.com>
2023-10-26 15:40:12 -0600

    Add reboot key to adjust layer

--- keyboards/keebio/iris/keymaps/mcfarlandj/config.h
+++ keyboards/keebio/iris/keymaps/mcfarlandj/config.h
@@ -19,3 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
+// Try tuning for better shift/enter
+#define HOLD_ON_OTHER_KEY_PRESS
